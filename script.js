67750bc1a69a2056641fd73997a8fa28312ba710
Sangeetha <89621488+Sangeetha19057@users.noreply.github.com>
2021-09-23 12:17:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-//---------------------calculator starts here------------------------------------
+//  calculator starts here
+const loader_div = document.createElement("div");
+loader_div.setAttribute("class", "loader");
+const loader_image = document.createElement("img");
+loader_image.setAttribute(
+  "src",
+  "https://cdn.dribbble.com/users/1186261/screenshots/3718681/_______.gif"
+);
+loader_div.append(loader_image);
+document.querySelector(".page_loader").append(loader_div);
 
-const nav = document.createElement("nav");//create nav
-const taga = document.createElement("a");//create a
-const span = document.createElement("span");//create span
-span.innerHTML="My calc";//add text to span
-taga.append(span);//append span to a tag
-nav.append(taga);//append nav to a tag
+const main = document.createElement("section");
+main.setAttribute("class", "main");
+const nav = document.createElement("nav"); //create nav
+const taga = document.createElement("a"); //create a
+const span = document.createElement("span"); //create span
+span.innerHTML = "My calc"; //add text to span
+taga.append(span); //append span to a tag
+nav.append(taga); //append nav to a tag
 
 let section = document.createElement("section");
-section.setAttribute("class","sec");
-const calculator_Box =  document.createElement("div");
-calculator_Box.setAttribute("class","calculator-Box");
+section.setAttribute("class", "sec");
+const calculator_Box = document.createElement("div");
+calculator_Box.setAttribute("class", "calculator-Box");
 
-let togglediv =document.createElement("div");
-togglediv.setAttribute("class","toggle");
+let togglediv = document.createElement("div");
+togglediv.setAttribute("class", "toggle");
 
 const calculator_Box__container = document.createElement("div");
-calculator_Box__container.setAttribute("class","calculator-Box__container");
-calculator_Box__container.innerHTML=`
+calculator_Box__container.setAttribute("class", "calculator-Box__container");
+
+calculator_Box__container.innerHTML = `
 
 <input onkeypress="return isNumberKey(event)"  type="text" id="inputext" placeholder="0">
+<!-- use onclick event -->
+<!-- while passing the arguments in the function use single ' ' not " " -->
+ 
+
+
+
+
   <button onclick="clearbutton()">C</button>
   <button onclick="deletebutton()">DEL</button>
   <button onclick="calculator('%')">%</button>
@@ -41,81 +60,89 @@ calculator_Box__container.innerHTML=`
   <button class="calculator-Box__container--equal" onclick="Result()">=</button>
 `;
 
-calculator_Box.append(togglediv,calculator_Box__container);
+calculator_Box.append(togglediv, calculator_Box__container);
 section.append(calculator_Box);
-document.body.append(nav);
-document.body.append(section);
-//-----------------calculator ENDS here-----------------------------------------
+main.append(nav, section);
+document.body.append(main);
+//-----------------calculator ENDS here---------------------------
 
-//----------------scrpit for toggle----------------------------------------------
-const sec = document.querySelector('.sec');
-const toggle = document.querySelector('.toggle');
-toggle.onclick = function()
-{
-    sec.classList.toggle('dark');
-}
-//----------------scrpit for toggle-----------------------------------------------
+//----------------scrpit for toggle----------------------------
+const sec = document.querySelector(".sec");
+const toggle = document.querySelector(".toggle");
+toggle.onclick = function () {
+  sec.classList.toggle("dark");
+};
+//----------------scrpit for toggle----------------------------
 
 // script starts here
 let result = document.getElementById("inputext");
+
 //--------------------only numbers are allowed------------------------------------
-function isNumberKey(evt)
-{
-   var charCode = (evt.which) ? evt.which : event.keyCode
-   if (charCode > 31 && (charCode < 48 || charCode > 57))
-   {
+function isNumberKey(evt) {
+  var charCode = evt.which ? evt.which : event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
     alert("only numbers are allowed");
     return false;
-   }
-    else{
-        return true;
-    }
-
-}
-
-//--------------------calculator-------------------------------------------------
-let calculator = (number) =>{      
-    result.value = result.value + number;
-}
-
-//--------------------result-----------------------------------------------------
-let Result = () =>{
-if(result.value === "")
-{
-    alert("please! Enter the Input to perform the calculation.")
-    return false;
-}
-try{
-    result.value=eval(result.value)
-  }
-  catch(err)
-  {
-      alert("please! Enter the valid Input")
-      result.value = "";
-
+  } else {
+    return true;
   }
 }
 
+// const numb=213231221;
 
-//---------------------clear button function---------------------------------------
-function clearbutton()
-{
-    if(result.value === "")
-{
-    alert("All the numbers are cleared.")
-    return false;
+function separator(numb) {
+  console.log(numb);
+  var str = numb.toString().split(".");
+  str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return str.join(".");
 }
+//calculator
+
+let calculator = (number) => {
+  result.value = result.value + number;
+};
+
+//reslut
+let Result = () => {
+  if (result.value === "") {
+    alert("please! Enter Input to perform the calculation.");
+    return false;
+  }
+  try {
+    result.value = eval(result.value);
+  } catch (err) {
+    alert("please! Enter the valid Input");
     result.value = "";
+  }
+};
+// clear button function
+function clearbutton() {
+  if (result.value === "") {
+    alert("All the numbers are cleared.");
+    return false;
+  }
+  result.value = "";
 }
 
-//----------------------delete button function------------------------------------
-function deletebutton()
-{
-    if(result.value === "")
-    {
-        alert("All the numbers are deleted.")
-        return false;
-    }
-    // slice method as two para( start and end)
-    result.value = result.value.slice(0,-1);
+// delete button function
+function deletebutton() {
+  if (result.value === "") {
+    alert("All the numbers are deleted.");
+    return false;
+  }
+  // slice method as two para( start and end)
+  result.value = result.value.slice(0, -1);
 }
+
+// loader script
+const loader = document.querySelector(".loader");
+const main_content = document.querySelector(".main");
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = "none";
+
+    main_content.style.display = "block";
+    setTimeout(() => (main_content.style.opacity = 1), 50);
+  }, 4000);
+});
